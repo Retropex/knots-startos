ab693fa484ae4b5fc012a87dded088a2c475719a
copy2018 <144347307+copy2018@users.noreply.github.com>
2025-03-07 08:38:34 +0000

    Update getConfig.ts to make v2transport protocol default

--- scripts/services/getConfig.ts
+++ scripts/services/getConfig.ts
@@ -416,7 +416,7 @@ export const getConfig: T.ExpectedExports.getConfig = async (effects) => {
               type: "boolean",
               name: "Use V2 P2P Transport Protocol",
               description: "Enable or disable the use of BIP324 V2 P2P transport protocol.",
-              default: false,
+              default: true,
             },
             addnode: {
               name: "Add Nodes",
